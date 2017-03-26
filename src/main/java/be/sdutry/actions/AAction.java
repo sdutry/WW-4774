c845a8fc912ff787faf45fbe082dc4f85788862f
Stefaan Dutry <stefaan.dutry@gmail.com>
2017-03-26 13:25:36 +0200

    change A to use redirect result to go to B

--- src/main/java/be/sdutry/actions/AAction.java
+++ src/main/java/be/sdutry/actions/AAction.java
@@ -2,5 +2,11 @@ package be.sdutry.actions;
 
 import com.opensymphony.xwork2.ActionSupport;
 
+import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.convention.annotation.Results;
+
+@Results({
+  @Result(name=ActionSupport.SUCCESS, location="/b", type="redirect")
+})
 public class AAction extends ActionSupport {
 }
