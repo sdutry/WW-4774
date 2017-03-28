67e1b2907722050f3e048a1ac1978a981d9d611b
Stefaan Dutry <stefaan.dutry@gmail.com>
2017-03-28 17:19:52 +0200

    testing SslOffloadAwareServletRedirectResult

--- src/main/java/be/sdutry/actions/AAction.java
+++ src/main/java/be/sdutry/actions/AAction.java
@@ -2,11 +2,13 @@ package be.sdutry.actions;
 
 import com.opensymphony.xwork2.ActionSupport;
 
+import org.apache.struts2.convention.annotation.ParentPackage;
 import org.apache.struts2.convention.annotation.Result;
 import org.apache.struts2.convention.annotation.Results;
 
+@ParentPackage("ssl-offload-default")
 @Results({
-  @Result(name=ActionSupport.SUCCESS, location="/b", type="redirect")
+  @Result(name=ActionSupport.SUCCESS, location="/b", type="sslOffloadRedirect")
 })
 public class AAction extends ActionSupport {
 }
